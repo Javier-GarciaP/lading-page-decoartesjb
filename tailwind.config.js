fd95382b408699344dd9d02121f744d03bcde493
José Javier García <josejaviergarciap123@gmail.com>
2025-03-21 12:41:46 -0400

    Actualizacion de la web

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}", // Ajusta según la ubicación de tus archivos
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
